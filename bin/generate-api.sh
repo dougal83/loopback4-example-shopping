49c0a6dee740785a303d80e91d654bd44f7f6a5d
Hage Yaapa <hage.yaapa@in.ibm.com>
2019-12-23 16:53:15 +0530

    feat: example shopping app website
    Added example shopping app website
    
    Signed-off-by: Hage Yaapa <hage.yaapa@in.ibm.com>

---
+++ bin/generate-api.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+### Generated code formatting ###
+
+# Requires Prettier global install https://prettier.io/docs/en/install.html
+
+# Enable for your OS or disable by commenting out the line below containing `-enable-post-process-file`
+
+## Linux (tested on Ubuntu)
+export TS_POST_PROCESS_FILE="/usr/bin/prettier --write"
+## Mac (untested)
+# export TS_POST_PROCESS_FILE="/usr/local/lib/node_modules/prettier --write"
+## Win (tested on Win10)
+# export TS_POST_PROCESS_FILE="$APPDATA/npm/prettier.cmd --write"
+
+
+echo -e "\n\e[96mAPI client code generator started...\n\e[39m";
+
+openapi-generator generate \
+    -i http://127.0.0.1:3000/openapi.json \
+    -g typescript-angular \
+    -o packages/client/src/app/core/api \
+    --enable-post-process-fileg
+
+echo -e "\n\e[92mAPI client code generator completed.\n";
